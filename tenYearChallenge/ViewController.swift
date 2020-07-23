d6b141ed26d99386136110497a5cfc6501dd44e0
Baaro <show0007@msn.com>
2020-07-23 10:03:05 +0800

    Initial Commit

---
+++ tenYearChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tenYearChallenge
+//
+//  Created by 林家宇 on 2020/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
